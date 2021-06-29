1993eeeb33b92e0142cfa79cd80542fe58b4bfa6
yampi-script <76887870+yampi-script@users.noreply.github.com>
2021-06-29 11:52:21 -0500

    ok

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
 
+
+
 require_once "controladores/plantilla.controlador.php";
 require_once "controladores/usuarios.controlador.php";
 require_once "controladores/categorias.controlador.php";
